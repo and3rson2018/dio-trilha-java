4acbcd682c4cfb0138d98021b57f1feda55c553f
and3rson2018 <and3rson.m@hotmail.com>
2024-09-18 15:50:45 -0300

    acrescentar  aceita um cafezinho

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -2,5 +2,6 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Prazer em conhecer você!");
+        System.out.println("Aceita um cafezinho?");
     }
 }
